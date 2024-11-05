9b0457de16f08aeb6f38739df40a2813e047e7b8
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-11-04 20:13:07 -0800

    medium level difficulty

---
+++ 7_patterns.py
@@ -0,0 +1,16 @@
+'''
+- - - - * - - - - 
+- - - * * * - - - 
+- - * * * * * - - 
+- * * * * * * * - 
+* * * * * * * * * 
+'''
+#replace "-" with " " 
+for i in range(5):
+    for j in range(5- i - 1):
+        print("-", end=" ")
+    for j in range(2*i + 1):
+        print("*", end=" ")
+    for j in range(5 - i - 1):
+        print("-", end=" ")
+    print()
