701d3d19388f525201dcfd5ffbf4dbcc66d483ac
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-11-06 12:40:17 -0800

    Update 11_Patterns.py

--- 11_Patterns.py
+++ 11_Patterns.py
@@ -1,1 +1,18 @@
+'''
+1 
+0 1 
+1 0 1 
+0 1 0 1 
+'''
+#replace 5 with dynamic input
 
+for i in range(5):
+    if i % 2==0:
+        start = 0
+    else:
+        start = 1
+    
+    for j in range(i):
+        print(start, end=" ")
+        start = 1 - start 
+    print()
