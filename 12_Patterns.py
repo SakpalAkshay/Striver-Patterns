73216153a92c05c8138a0957eb35fef6cd0a4837
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-11-06 12:50:26 -0800

    pushed code

--- 12_Patterns.py
+++ 12_Patterns.py
@@ -1,1 +1,23 @@
+'''
+0 - - - - - - 0 
+0 1 - - - - 1 0 
+0 1 2 - - 2 1 0 
+0 1 2 3 3 2 1 0
+'''
 
+#replace 4 with dynamic input
+count = 2*(4-1)
+for i in range(4):
+
+    for j in range(i+1):
+        print(j, end=" ")
+    
+    for j in range(count):
+        print("-", end=" ")
+
+    for j in range(i,-1,-1):
+        print(j, end=" ")
+
+    count -=2
+
+    print()  
