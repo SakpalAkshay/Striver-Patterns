a77435989dac9db759cac16ec6fe0cc6578716fd
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-11-26 17:37:48 -0800

    Update 1-Largest-Element.py

--- Arrays/Easy/1-Largest-Element.py
+++ Arrays/Easy/1-Largest-Element.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def getLargest(self, arr):
+        # Code Here
+    
+        largest = arr[0]
+    
+        for i in range(1,len(arr)):
+            
+            if arr[i] > largest:
+                largest = arr[i]
+        return largest
