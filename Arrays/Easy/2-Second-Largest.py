526abee27a2778ec790b848a0f8eab973801df24
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-11-26 17:38:28 -0800

    pushed solution 1

--- Arrays/Easy/2-Second-Largest.py
+++ Arrays/Easy/2-Second-Largest.py
@@ -1,1 +1,19 @@
+class Solution:
+    def getSecondLargest(self, arr):
+        # Code Here
+    
+        largest = arr[0]
+    
+        for i in range(1,len(arr)):
+            
+            if arr[i] > largest:
+                largest = arr[i]
+        
+        secondLargest = -1
+        
+        for i in range(len(arr)):
+            if arr[i] > secondLargest and arr[i] != largest:
+                secondLargest = arr[i]
+        
+        return secondLargest
 
